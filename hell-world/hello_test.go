be2e584e27a882e77961800c17c052fa25fc540c
Masahiko Sato <sato_m@mediaxis.jp>
2018-07-29 15:05:11 +0900

    add hello-world

---
+++ hell-world/hello_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("masa", "English")
+		want := "Hello, masa"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("", "English")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("masa", "French")
+		want := "Bonjour, masa"
+		assertCorrectMessage(t, got, want)
+	})
+}
